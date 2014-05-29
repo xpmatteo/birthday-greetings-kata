44e2b199f2c46e17d46097f19e5829c1e6b7c0c5
Matteo Vaccari <vaccari@pobox.com>
2014-05-29 22:01:24 +0200

    Reorganized libraries and Eclipse project files

--- src/test/java/xpug/kata/birthday_greetings/XDateTest.java
+++ src/test/java/xpug/kata/birthday_greetings/XDateTest.java
@@ -1,4 +1,5 @@
 package xpug.kata.birthday_greetings;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -12,25 +13,25 @@ public class XDateTest {
 		assertEquals(1, xDate.getMonth());
 		assertEquals(24, xDate.getDay());
 	}
-	
+
 	@Test
 	public void isSameDate() throws Exception {
 		XDate xDate = new XDate("1789/01/24");
 		XDate sameDay = new XDate("2001/01/24");
 		XDate notSameDay = new XDate("1789/01/25");
 		XDate notSameMonth = new XDate("1789/02/25");
-		
+
 		assertTrue("same", xDate.isSameDay(sameDay));
 		assertFalse("not same day", xDate.isSameDay(notSameDay));
 		assertFalse("not same month", xDate.isSameDay(notSameMonth));
 	}
-	
+
 	@Test
 	public void equality() throws Exception {
 		XDate base = new XDate("2000/01/02");
 		XDate same = new XDate("2000/01/02");
 		XDate different = new XDate("2000/01/04");
-		
+
 		assertFalse(base.equals(null));
 		assertFalse(base.equals(""));
 		assertTrue(base.equals(base));
